be17ad85c80d26dcf1a6dfe8a8133657124e22f0
danilojduarte <76825919+danilojduarte@users.noreply.github.com>
2024-08-26 01:01:14 -0300

    Aplicando javascript

--- Js/script.js
+++ Js/script.js
@@ -1,4 +1,22 @@
 /*=============== SHOW MENU ===============*/
+const navMenu = document.getElementById('nav__menu'),
+      navToggle = document.getElementById('nav__toggle'),
+      navClose = document.getElementById('nav__close')
+
+      // Menu Show
+      if(navToggle){
+        navToggle.addEventListener('click', () => {
+          navMenu.classList.add('show-menu')
+        })
+      }
+
+      // Menu Hidden
+
+      if(navClose) {
+        navClose.addEventListener('click', () => {
+          navMenu.classList.remove('show-menu')
+        })
+      }
 
 
 /*=============== REMOVE MENU MOBILE ===============*/
